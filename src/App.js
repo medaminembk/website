281d212fa0acbc6b4d26dd3445a3d0c08a6c8de1
medaminembk <mbarekmouhamedamine@gmail.com>
2022-01-02 16:48:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/form/form';
+function App() {
+  return (
+    <div className="App">
+     <Form title="Contact form" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = () => {
+    return (
+        <div>
+            hi dataaa
+        </div>
+    )
+}
+
+export default Layout
